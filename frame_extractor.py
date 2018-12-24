7316e28e6a6c08ca29313ad8993d0e6c4d978665
gargdindia <gargdindia@hotmail.com>
2018-12-24 14:13:03 +0530

    added not woeking frame_extractor.py

---
+++ frame_extractor.py
@@ -0,0 +1,37 @@
+# Program To Read video 
+# and Extract Frames 
+import cv2 
+import sys
+  
+# Function to extract frames 
+def FrameCapture(path): 
+      
+    # Path to video file 
+    vidObj = cv2.VideoCapture(path) 
+  
+    # Used as counter variable 
+    count = 0
+    frameCount = 0
+  
+    # checks whether frames were extracted 
+    success = 1
+  
+    while success: 
+  
+        # vidObj object calls read 
+        # function extract frames 
+        success, image = vidObj.read() 
+  
+        if count % 6 == 0:
+            # Saves the frames with frame-count 
+            destination = sys.argv[2] + "frame" + str(frameCount)
+            cv2.imwrite(destination, image) 
+            frameCount += 1
+  
+        count += 1
+  
+# Driver Code 
+if __name__ == '__main__': 
+  
+    # Calling the function 
+    FrameCapture(sys.argv[1]) 

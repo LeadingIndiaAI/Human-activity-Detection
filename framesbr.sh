ef94659e7f5f2fb27846812a686a6cbf7c7b70c9
Adarsh <contactadarshk@gmail.com>
2019-01-02 15:06:40 +0530

    add framesbr.sh

---
+++ framesbr.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+DIR=`pwd`
+cd dataset
+for DATASET in `ls`
+do
+  for CLASS in `ls`
+  do
+    COUNT=1
+    for VIDEO in `ls $CLASS`
+    do
+      VID=$(echo "$COUNT.avi" | cut -d'.' -f 1)    
+      python $DIR/frame_extractor_br.py $DIR/dataset/$CLASS/$VIDEO $DIR/framesBR/$CLASS/$VID
+      (( COUNT++ ))
+    done
+  done
+done

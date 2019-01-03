f77f7c7c357d3d108aaa790ea83b384bbfee77bb
Adarsh <contactadarshk@gmail.com>
2019-01-03 21:23:55 +0530

    fix bug

--- scripts/framesbr.sh
+++ scripts/framesbr.sh
@@ -3,13 +3,13 @@ DIR=`pwd`
 cd dataset
 for DATASET in `ls`
 do
-  for CLASS in `ls`
+  for CLASS in `ls $DATASET`
   do
     COUNT=1
-    for VIDEO in `ls $CLASS`
+    for VIDEO in `ls $DATASET/$CLASS`
     do
       VID=$(echo "$COUNT.avi" | cut -d'.' -f 1)    
-      python $DIR/scripts/frame_extractor_br.py $DIR/dataset/$CLASS/$VIDEO $DIR/framesBR/$CLASS/$VID
+      python $DIR/scripts/frame_extractor_br.py $DIR/dataset/$DATASET/$CLASS/$VIDEO $DIR/frames/$DATASET/$CLASS/$VID
       (( COUNT++ ))
     done
   done
